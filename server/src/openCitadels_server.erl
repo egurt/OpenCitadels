2b2fb82d8ba8bdef4acdcba3d30c4187fbc65cb6
Samuel Strand <samuel.strand@gmail.com>
2012-08-07 02:37:11 +0200

    Preliminary implementation 	* state records 	* setup/2 	* register/1 	* listplayers/1 	* move/4 	* send/3

--- server/src/openCitadels_server.erl
+++ server/src/openCitadels_server.erl
@@ -1,13 +1,23 @@
 -module(openCitadels_server).
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
+-define(GAME, openCitadels_game).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0, setup/1, register/0, quit/0,
-	 players/0, status/0, status/1, move/2]).
+-export([ start_link/0
+        , setup/2
+        , register/1
+        , list_players/1
+        , state/0
+        , status/0
+        , game_status/1
+        , game_status/2
+        , move/4
+        , send/3
+        ]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -16,6 +26,20 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+%% ----
+%% Record definitions
+%% ----
+-record(state, { games = []   %% #game
+               , players = [] %% #player
+               }).
+
+-record(game, { id
+              , pid
+              , players = [] %% id
+              }).
+-record(player, { id
+                , pid
+                }).
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -23,32 +47,32 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, no_args, []).
 
-setup(ClientID) ->
-    gen_server:call(?SERVER, {setup, ClientID}).
+setup(ClientIDs, Options) ->
+    gen_server:call(?SERVER, {setup, ClientIDs, Options}).
 
-register() ->
-    ok.
+register(Pid) ->
+    gen_server:call(?SERVER, {register, Pid}).
 
-quit() ->
-    ok.
+list_players(GameID) ->
+    gen_server:call(?SERVER, {list_players, GameID}).
 
-players() ->
-    ok.
+state() ->
+    gen_server:call(?SERVER, state).
 
 status() ->
     ok.
 
-status(Player) ->
+game_status(_GameID) ->
     ok.
 
-move(Type, Args) ->
+game_status(_GameID, _Player) ->
     ok.
 
-%% ----
-%% State definition
-%% ----
--record(state, {
-               }).
+move(GameID, PlayerID, Move, Args) ->
+    gen_server:cast(?SERVER, {move, GameID, PlayerID, Move, Args}).
+
+send(GameID, PlayerID, Message) ->
+    gen_server:cast(?SERVER, {send, GameID, PlayerID, Message}).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
@@ -58,12 +82,60 @@ init(no_args) ->
     {ok, #state{}}.
 
 
-handle_call({setup, ClientID}, _From, State) ->
-    {reply, ok, State};
+handle_call({register, Pid}, {Pid, _Tag} = _From, State) ->
+    ID = erlang:now(),
+    Player = #player{id = ID, pid = Pid},
+    Players = [Player | State#state.players],
+    {reply, {ok, ID}, State#state{players = Players}};
+
+handle_call({setup, PlayerIDs, Options}, _From, State) ->
+    Players = [  lists:keyfind(ID, #player.id, State#state.players)
+              || ID <- PlayerIDs],
+    case lists:member(false, Players) of
+        true ->
+            {reply, {error, 'unregistered player'}, State};
+        false ->
+            GameID = erlang:now(),
+            %% use supervisor?
+            {ok, GamePid} = ?GAME:start_link([ {id, GameID}
+                                             , {players, PlayerIDs}
+                                             | Options
+                                             ]),
+            NewGame = #game{ id = GameID 
+                           , pid = GamePid
+                           , players = PlayerIDs
+                           },
+            Games = [NewGame | State#state.games],
+            {reply, {ok, GameID}, State#state{games = Games}}
+    end;
+
+handle_call({list_players, GameID}, _From, State) ->
+    Reply = case gamefind(GameID, State) of
+        false -> {error, 'wrong id'};
+        Game  -> {ok, Game#game.players}
+    end,
+    {reply, Reply, State};
+
+handle_call(state, _From, State) ->
+    {reply, State, State};
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
+
+handle_cast({move, GameID, PlayerID, Move, Data}, State) ->
+    Pid = (gamefind(GameID, State))#game.pid,
+    ?GAME:Move(Pid, PlayerID, Data),
+    {noreply, State};
+
+handle_cast({send, GameID, all, Message}, State) ->
+    PlayerIDs = (gamefind(GameID, State))#game.players,
+    sendplayers(PlayerIDs, {message, GameID, Message}, State),
+    {noreply, State};
+
+handle_cast({send, GameID, PlayerID, Message}, State) ->
+    sendplayers([PlayerID], {message, GameID, Message}, State),
+    {noreply, State};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -83,4 +155,12 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+gamefind(GameID, State) ->
+    lists:keyfind(GameID, #game.id, State#state.games).
 
+sendplayers(PlayerIDs, Message, State) ->
+    Fun = fun (ID) ->
+        Pid = (lists:keyfind(ID, #player.id, State#state.players))#player.pid,
+        Pid ! Message
+    end,
+    lists:foreach(Fun, PlayerIDs).
