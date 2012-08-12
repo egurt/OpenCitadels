6465e866fee30db04a5d7c21573e7fdc95c6be4f
William Sjöstedt <william.sjostedt@gmail.com>
2012-08-12 14:31:46 +0200

    Added pre_play_init

--- server/src/openCitadels_game.erl
+++ server/src/openCitadels_game.erl
@@ -4,26 +4,27 @@
 -compile(export_all). %% Fix this later!
 
 -record(player_state,
-	{player_id %The identifier of the current player
-	 ,current_character = none %The character this player is
-	 ,districts = [] %The districts the player has built
-	 ,hand = [] %The cards in the players hand
-	 ,money = 2 %The amount of money a player has
-	 ,effects = [] %assassinated, stolen from, other?
-	}).
+        {player_id %The identifier of the current player
+         ,current_character = none %The character this player is
+         ,districts = [] %The districts the player has built
+         ,hand = [] %The cards in the players hand
+         ,money = 2 %The amount of money a player has
+         ,effects = [] %assassinated, stolen from, other?
+        }).
 
 -record(game_state,
-	{game_id %How a game instance is identified by the server (static)
-	 ,server_pid %For communicating errors and the like (static)
-	 ,player_order = [] %The order the players go (static)
-	 ,current_player = 1 %The current player, relative to player_order
-	 ,players = [] %List of player states
-	 ,first_player = 1 %First player (is changed after a round)
-	 ,district_deck = [] %Deck of districts
-	 ,character_deck = [] %Deck of characters to select from
-	 ,face_down = [] %The face down characters
-	 ,face_up = [] %The face up characters
-	}).
+        {game_id %How a game instance is identified by the server (static)
+         ,server_pid %For communicating errors and the like (static)
+         ,player_order = [] %The order the players go (static)
+         ,current_player = 1 %The current player, relative to player_order
+         ,character_order = [] %During play, shows the selected characters' order
+         ,players = [] %List of player states
+         ,first_player = 1 %First player (is changed after a round)
+         ,district_deck = [] %Deck of districts
+         ,character_deck = [] %Deck of characters to select from
+         ,face_down = [] %The face down characters
+         ,face_up = [] %The face up characters
+        }).
 
 %% ------------------------------------------------------------------
 %% gen_fsm Function Definitions
@@ -49,38 +50,43 @@ take_card(Pid, Player, Card) ->
 %% ------------------------------------------------------------------
 
 deal_cards({take_card, Player, Card}, 
-	   #game_state{current_player = CurrentPlayer
-		       ,players = Players
-		       ,player_order = PlayerOrder
-		       ,character_deck = CDeck
-		       ,first_player = FirstPlayer} = State) ->
+           #game_state{current_player = CurrentPlayer
+                       ,players = Players
+                       ,player_order = PlayerOrder
+                       ,character_deck = CDeck
+                       ,first_player = FirstPlayer} = State) ->
 
     CurrentPlayerID = get_player_id(CurrentPlayer, PlayerOrder),
     case {CurrentPlayerID =:= Player,
-	  lists:member(Card, CDeck)} of
-	{true, true} -> %Correct player's turn, card is available
-	    PlayerState = get_player_state(CurrentPlayerID, State),
-	    NewPlayerState = 
-		PlayerState#player_state{current_character = Card},
-	    OtherPlayers = lists:delete(PlayerState, Players),
-	    NewDeck = lists:delete(Card, CDeck),
-	    NextPlayer = (CurrentPlayer rem length(PlayerOrder)) + 1,
-	    NewState = State#game_state{current_player = NextPlayer
-				       ,players = [NewPlayerState | OtherPlayers]
-				       ,character_deck = NewDeck},
+          lists:member(Card, CDeck)} of
+        {true, true} -> %Correct player's turn, card is available
+            PlayerState = get_player_state(CurrentPlayerID, State),
+            NewPlayerState = 
+                PlayerState#player_state{current_character = Card},
+            OtherPlayers = lists:delete(PlayerState, Players),
+            NewDeck = lists:delete(Card, CDeck),
+            NextPlayer = (CurrentPlayer rem length(PlayerOrder)) + 1,
+            NewState = State#game_state{current_player = NextPlayer
+                                       ,players = [NewPlayerState | OtherPlayers]
+                                       ,character_deck = NewDeck},
 
-	    if NextPlayer =/= FirstPlayer -> 
-		    {next_state, deal_cards, NewState};
-	       true -> {next_state, deal_cards, State} %For debugging?
-		       %play(NewState)
-	    end;
+            if NextPlayer =/= FirstPlayer -> 
+                    {next_state, deal_cards, NewState};
+               true -> play(NewState)
+            end;
 
-	_ -> {next_state, deal_cards, State}
+        _ -> {next_state, deal_cards, State}
     end.
 
-%play(#game_state{} = State) ->
-    
-
+pre_play_init(#game_state{players = Players} = State) ->
+    %Could possibly just sort the characters, depending on representation
+    Fun = fun(#player_state{current_character = C, player_id = P}) -> {C, P} end,
+    Selected = lists:map(Fun, Players),
+    %If cards can be sorted in correct order, this would not be necessary
+    SelectedInOrder = 
+	lists:sort(fun ({C1, _}, {C2, _}) -> character_order(C1, C2) end, Selected),
+    NewState = State#game_state{character_order = SelectedInOrder},
+    NewState.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
@@ -90,16 +96,16 @@ init_state(Options) ->
     DistrictDeck = shuffle_deck(district_list()),
     init_state(Options, #game_state{district_deck = DistrictDeck}).
 init_state([], State) ->
-    State;
+    pre_deal_cards(State);
 init_state([{players, Players} | Options], 
-	   #game_state{district_deck = DistrictDeck} = State) ->
+           #game_state{district_deck = DistrictDeck} = State) ->
     {PlayerStates, DDeck} = initialise_player_states(Players, DistrictDeck),
     NewState = State#game_state{players = PlayerStates
-				,district_deck = DDeck
-				,player_order = Players
-				,first_player = 1
-				,current_player = 1
-			       },
+                                ,district_deck = DDeck
+                                ,player_order = Players
+                                ,first_player = 1
+                                ,current_player = 1
+                               },
     init_state(Options, NewState);
 init_state([{server_pid, ServerPid} | Options], State) ->
     NewState = State#game_state{server_pid = ServerPid},
@@ -128,7 +134,7 @@ get_player_id(Player, PlayerOrder) ->
 get_player_state(PlayerID, #game_state{players = Players}) ->
     lists:keyfind(PlayerID, #player_state.player_id, Players).
 %    {[PlayerState], _} = 
-%	lists:partition(fun(P) -> P#player_state.player_id =:= PlayerID end, Players).
+%       lists:partition(fun(P) -> P#player_state.player_id =:= PlayerID end, Players).
 
 % Initialise gamestate for dealing character cards
 pre_deal_cards(GameState) ->
@@ -136,14 +142,14 @@ pre_deal_cards(GameState) ->
 pre_deal_cards(GameState, 5) ->
     [FaceDown , FaceUp , MaybeFaceUp | CDeck] = shuffle_deck(character_list()),
     case FaceUp of
-	{4, king} ->
-	    GameState#game_state{character_deck = [FaceUp | CDeck]
-				 ,face_down = [FaceDown]
-				 ,face_up = [MaybeFaceUp]};
-	_ ->
-	    GameState#game_state{character_deck = [MaybeFaceUp | CDeck]
-				 ,face_down = [FaceDown]
-				 ,face_up = [FaceUp]}
+        {4, king} ->
+            GameState#game_state{character_deck = [FaceUp | CDeck]
+                                 ,face_down = [FaceDown]
+                                 ,face_up = [MaybeFaceUp]};
+        _ ->
+            GameState#game_state{character_deck = [MaybeFaceUp | CDeck]
+                                 ,face_down = [FaceDown]
+                                 ,face_up = [FaceUp]}
     end;
 pre_deal_cards(_, _) ->
     {error, "No rules for this amount of players."}.
@@ -161,6 +167,10 @@ num_players(#game_state{player_order = PlayerOrder}) ->
 %% Test Functions
 %% ------------------------------------------------------------------
 
+% Sort characters function
+character_order(C1, C2) ->
+    C1 =< C2.
+
 % Get list of characters in the game
 character_list() ->
     [{1, assassin}
@@ -175,7 +185,7 @@ character_list() ->
 % Get list of district cards in the game
 district_list() ->
     Base = [{X, Y} || X <- [1,2,3,4,5],
-		      Y <- [green, yellow, red, blue]],
+                      Y <- [green, yellow, red, blue]],
     Purple = [{X, purple} || X <- [3,4,5,6]],
     Base ++ Base ++ Purple ++ Purple.
 
@@ -186,34 +196,33 @@ option_list() ->
     ].
 
 test() ->
-    EmptyState = init_state(option_list()),
-    InitState = pre_deal_cards(EmptyState, num_players(EmptyState)),
+    InitState = init_state(option_list()),
     [First, Second, Third, Fourth, Fifth | Rest] = 
-	InitState#game_state.character_deck,
+        InitState#game_state.character_deck,
     {_, _, NewState1} =
-	deal_cards({take_card, 
-		    get_player_id(InitState#game_state.current_player, InitState), 
-		    First},
-		   InitState),
+        deal_cards({take_card, 
+                    get_player_id(InitState#game_state.current_player, InitState), 
+                    First},
+                   InitState),
     {_, _, NewState2} =
-	deal_cards({take_card, 
-		    get_player_id(NewState1#game_state.current_player, NewState1), 
-		    Second},
-		   NewState1),
+        deal_cards({take_card,
+                    get_player_id(NewState1#game_state.current_player, NewState1), 
+                    Second},
+                   NewState1),
     {_, _, NewState3} =
-	deal_cards({take_card, 
-		    get_player_id(NewState2#game_state.current_player, NewState2), 
-		    Third},
-		   NewState2),
+        deal_cards({take_card, 
+                    get_player_id(NewState2#game_state.current_player, NewState2), 
+                    Third},
+                   NewState2),
     {_, _, NewState4} =
-	deal_cards({take_card, 
-		    get_player_id(NewState3#game_state.current_player, NewState3), 
-		    Fourth},
-		   NewState3),
+        deal_cards({take_card, 
+                    get_player_id(NewState3#game_state.current_player, NewState3), 
+                    Fourth},
+                   NewState3),
     %{_, _, NewState} =
-	deal_cards({take_card, 
-		    get_player_id(NewState4#game_state.current_player, NewState4), 
-		    Fifth},
-		   NewState4).
+        deal_cards({take_card, 
+                    get_player_id(NewState4#game_state.current_player, NewState4), 
+                    Fifth},
+                   NewState4).
 
     %NewState.
