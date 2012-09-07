5fada352cc06650f50f2bf467ddc3630161ca56b
Samuel Strand <samuel.strand@gmail.com>
2012-09-07 02:53:28 +0200

    load client module on start up

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-erl -pa server/ebin -pa client/ebin -eval 'application:start(openCitadels_server).'
+erl -pa server/ebin -pa client/ebin -eval 'application:start(openCitadels_server),code:load_file(openCitadels_client).'
